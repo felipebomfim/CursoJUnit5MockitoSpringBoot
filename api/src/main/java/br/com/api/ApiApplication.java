46247f630e2f73d31247f3c768af6c64b5629949
Felipe Bomfim <felipe.bomfim@camara.leg.br>
2024-08-19 14:45:21 -0300

    Created User class

--- api/src/main/java/br/com/api/ApiApplication.java
+++ api/src/main/java/br/com/api/ApiApplication.java
@@ -3,11 +3,15 @@ package br.com.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.api.domain.User;
+
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+
+		User user = new User(1, "Felipe", "felipe@email.com", "senha123");
 	}
 
 }
